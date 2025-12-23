c08c5b2fcae3551bc2ab8027c8409c0858facf63
Amrit <amritendup@gmail.com>
2025-12-23 21:22:34 +0530

    environment setup

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+
+def main():
+    print("Hello from langchain-course!")
+
+
+if __name__ == "__main__":
+    main()
